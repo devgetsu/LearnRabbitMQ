fe01df01d3b3c8120f0922813b6128de4b426467
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-05-31 16:30:09 +0500

    Add project files.

---
+++ RabbitMQMessage.Reciever/Program.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Recieve();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+
+    static void Recieve()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        {
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("[x] Recieved {0}", message);
+                };
+
+                channel.BasicConsume(queue: "hello",
+                    autoAck: true,
+                    consumer: consumer);
+            }
+        }
+
+    }
+}
---
+++ RabbitMQMessage.Sender/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Send();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+
+    public static void Send()
+    {
+        var factory = new ConnectionFactory { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        {
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                string message = "Hello .NET!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                    routingKey: "hello",
+                    basicProperties: null,
+                    body: body);
+
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+        }
+    }
+
+}
